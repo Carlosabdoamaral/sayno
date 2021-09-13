bd724869402834137e240e2c4faa94bfa138ce85
Amaralzaao <72256470+Amaralzaao@users.noreply.github.com>
2021-09-12 21:24:11 -0300

    initial Commit

---
+++ SayNo/Shared/SayNoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayNoApp.swift
+//  Shared
+//
+//  Created by JEFERSON AMARAL on 12/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SayNoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
