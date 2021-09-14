4387fc8a2ddc909e89cbe86203429872633799e0
CARLOS ALBERTO BARCELOS DO AMARAL <carlos.a.amaral@sesisc.org.br>
2021-09-14 11:25:50 -0300

    Adding motivacional phrases

--- SayNo/Shared/Reports/ReportView.swift
+++ SayNo/Shared/Reports/ReportView.swift
@@ -16,17 +16,14 @@ struct ReportView: View {
     
     //Reagir
     @State var wantToReact : Bool = true
+
+    //Frase motivacional
+    @State var wantToLetAPhrase : Bool = true
+
     
-    let reactions = ["Vai dar tudo certo!",
-                     "Voce é incrível!",
-                     "Amei",
-                     "Voce não está sozinho(a)!",
-                     "Amei",
-                     "Apoio",
-                     "Amei",
-                     "Apoio",
-                     "Amei",
-                     "Apoio"]
+    let reactions = ["Vai dar tudo certo!", "Você é incrível!", "Amei", "Você não está sozinho(a)!", "Amei", "Apoio", "Amei", "Apoio", "Amei", "Apoio"]
+
+    let motivacionalPhrase = ["Amei", "Apoio", "Amei", "Amei", "Amei", "Amei"]
     
     func postComment(){
         print("Postou")
@@ -44,7 +41,8 @@ struct ReportView: View {
                 
                 Section(header: Text("Escreva uma mensagem positiva...")) {
                     Toggle("Deixar um comentário?", isOn: $wantToComment)
-                    Toggle("Deixar uma reacao", isOn: $wantToReact)
+                    Toggle("Deixar uma reação", isOn: $wantToReact)
+                    Toggle("Deixar uma frase motivacional", isOn: $wantToLetAPhrase)
                 }
                 
                 if wantToComment == true{
@@ -59,6 +57,50 @@ struct ReportView: View {
                     }
                 }
                 
+                if wantToLetAPhrase == true{
+                    Section(header: "Selecione uma frase motivacional"){
+                        VStack(alignment: .leading){
+                            ForEach(0..<10){i in
+                                VStack{
+                                    HStack{
+                                        
+                                        if i == 0{
+                                            Image(systemName: "face.smiling.fill")
+                                            Text("\(reactions[i])")
+                                                .onTapGesture {
+                                                    print("Selecionado \(i)")
+                                                }
+                                        }
+                                        
+                                        else if i == 1{
+                                            Image(systemName: "star.fill")
+                                            Text("\(reactions[i])")
+                                                .onTapGesture {
+                                                    print("Selecionado \(i)")
+                                                }
+                                        }
+                                        
+                                        else if i == 2{
+                                            Image(systemName: "suit.heart.fill")
+                                            Text("\(reactions[i])")
+                                                .onTapGesture {
+                                                    print("Selecionado \(i)")
+                                                }
+                                        }
+                                        
+                                        else if i == 3{
+                                            Image(systemName: "person.3.fill")
+                                            Text("\(reactions[i])")
+                                                .onTapGesture {print("Selecionado \(i)")}
+                                        }
+                                    }
+                                }
+                            Divider()
+                            }
+                        }
+                    }
+                }
+
                 if wantToReact == true{
                     VStack (alignment: .leading){
                         ForEach(0..<10){i in
