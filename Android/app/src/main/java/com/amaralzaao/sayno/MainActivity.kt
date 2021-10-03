53374e728a67d3fb5cd02b4671de2527c96d8d79
Amaralzaao <72256470+Amaralzaao@users.noreply.github.com>
2021-10-03 18:34:58 -0300

    tests nav android

--- Android/app/src/main/java/com/amaralzaao/sayno/MainActivity.kt
+++ Android/app/src/main/java/com/amaralzaao/sayno/MainActivity.kt
@@ -2,10 +2,20 @@ package com.amaralzaao.sayno
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.amaralzaao.sayno.R.layout.activity_main
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_report_details)
+        setContentView(activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
+
 }
