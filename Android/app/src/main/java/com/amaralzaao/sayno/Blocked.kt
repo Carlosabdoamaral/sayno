ace0cb2d0f3d29d4973c4c4587e512fa4af34b3b
Amaralzaao <72256470+Amaralzaao@users.noreply.github.com>
2021-10-03 03:45:23 -0300

    adding BlockedScreen

--- Android/app/src/main/java/com/amaralzaao/sayno/Blocked.kt
+++ Android/app/src/main/java/com/amaralzaao/sayno/Blocked.kt
@@ -3,7 +3,7 @@ package com.amaralzaao.sayno
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Blocked : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blocked)
