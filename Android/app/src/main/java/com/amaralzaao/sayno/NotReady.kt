a0bca156e9dd108c728124005f3167f4421dfcd7
Amaralzaao <72256470+Amaralzaao@users.noreply.github.com>
2021-10-03 03:35:39 -0300

    adding notReadyScreen android

--- Android/app/src/main/java/com/amaralzaao/sayno/NotReady.kt
+++ Android/app/src/main/java/com/amaralzaao/sayno/NotReady.kt
@@ -3,7 +3,7 @@ package com.amaralzaao.sayno
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class NotReady : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_not_ready)
