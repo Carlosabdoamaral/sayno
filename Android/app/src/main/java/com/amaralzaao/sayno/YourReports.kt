ac646aeabc740c39c2bfc230982af0d25e9b3ef1
Amaralzaao <72256470+Amaralzaao@users.noreply.github.com>
2021-10-03 04:51:24 -0300

    adding newReport and code to navigate

--- Android/app/src/main/java/com/amaralzaao/sayno/YourReports.kt
+++ Android/app/src/main/java/com/amaralzaao/sayno/YourReports.kt
@@ -1,11 +1,20 @@
 package com.amaralzaao.sayno
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class YourReports : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_your_reports)
+
+        val btn = findViewById<Button>(R.id.newBtn)
+        btn.setOnClickListener {
+            val intent = Intent(this,NewReport::class.java)
+            startActivity(intent)
+        }
     }
+
 }
