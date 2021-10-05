339c8d8e7176a8d090968fe7bd39c3b7c631491c
Amaralzaao <72256470+Amaralzaao@users.noreply.github.com>
2021-10-05 20:59:24 -0300

    Creating and adjusting screens Android

--- Android/app/src/main/java/com/amaralzaao/sayno/NewReport.kt
+++ Android/app/src/main/java/com/amaralzaao/sayno/NewReport.kt
@@ -2,6 +2,7 @@ package com.amaralzaao.sayno
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class NewReport : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
